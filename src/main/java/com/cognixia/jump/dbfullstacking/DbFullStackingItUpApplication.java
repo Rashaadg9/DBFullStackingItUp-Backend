a791cc65ce81eca5ee466c7f48f57cb0d7d3cffe
Rashaad Gray <grayrashaad@outlook.com>
2022-08-16 09:43:49 -0500

    inital commit

---
+++ src/main/java/com/cognixia/jump/dbfullstacking/DbFullStackingItUpApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump.dbfullstacking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbFullStackingItUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbFullStackingItUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognixia/jump/dbfullstacking/DbFullStackingItUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump.dbfullstacking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbFullStackingItUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
